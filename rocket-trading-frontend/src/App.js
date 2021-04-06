c43e92fde30a62098a048bace3953bd689374bf1
saslam23 <saadaslam13@gmail.com>
2021-04-06 18:39:21 -0500

    working on navbar

--- rocket-trading-frontend/src/App.js
+++ rocket-trading-frontend/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Navigation from './components/Navigation';
 
 function App() {
+
   return (
     <div className="App">
-    <h1>Rocket Trading</h1>
+    <Navigation/>
     </div>
   );
 }
---
+++ rocket-trading-frontend/src/components/Navigation.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+export default function Navigation() {
+    return (
+        <div className="navi">
+    <Navbar bg="#699aa4" variant="dark">
+    <li className="logo" href="#home">Rocket Trading</li>
+    <Nav style={{marginRight:'6rem'}} className="ml-auto">
+      <li className="nav-item" href="#home">Home</li>
+      <li className="nav-item" href="#features">About</li>
+      <li className="nav-item" href="#pricing">FAQ</li>
+      <li className="nav-item" href="#pricing">Contact</li>
+      <li className="nav-item" href="#pricing">Team</li>
+    </Nav>
+  </Navbar>
+        </div>
+    )
+}
