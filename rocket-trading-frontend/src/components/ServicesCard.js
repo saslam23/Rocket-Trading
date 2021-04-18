5ac08c0ea19bbc80625482c9c69627289d305810
saslam23 <saadaslam13@gmail.com>
2021-04-18 13:59:11 -0500

    services changed

--- rocket-trading-frontend/src/components/ServicesCard.js
+++ rocket-trading-frontend/src/components/ServicesCard.js
@@ -2,9 +2,22 @@ import React from 'react'
 
 export default function ServicesCard() {
     return (
-        <div>
+        <div className="service-card-control">
             <div className="service-card">
-            <h2 style={{color:'white', textAlign:'center'}}>this is a service</h2>
+            <h1 style={{color:'white',paddingTop:'1rem', textAlign:'center'}}>For $50/month <br/>You Get Access To:</h1>
+            <ul className="service-list-style">
+            <li>🚀  Live alerts from our Top Analyst</li>
+            <li>🚀  Access to all chatrooms</li>
+            <li>🚀  Weekly Watchlists</li>
+            <li>🚀  Trade alerts &#38; ideas</li>
+            <li>🚀  Trading strategy advice &#38; Risk Management</li>
+            <li>🚀  Active leadership and support</li>
+            <li>🚀  Larger Profit Signals</li>
+            <li>🚀  Advanced Research from our analyst</li>
+            <li>🚀  Crypto</li>
+            <li>🚀  Penny Stock</li>
+            <li>🚀  Long Term Plays</li>
+            </ul>
             </div>
         </div>
     )
